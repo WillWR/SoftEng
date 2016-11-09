9c9c1fddd559c8eaa70a6621137ca04b9db3b160
William Roberts <10017537@live.napier.ac.uk>
2016-11-09 21:21:11 +0000

    SMS 90% working, fix txtspeak replacement

--- ManualForm.xaml.cs
+++ ManualForm.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -50,12 +51,12 @@ namespace NBMFS
                 //EMAIL SENDER CHECK STARTS
                 if ((addSenderBox.Text != "") && (Regex.IsMatch(addSenderBox.Text, emailCheck)))
                 {
-                    String subjectCheck = @"^[a-zA-Z0-9.*]*${20}$";
+                    String subjectCheck = @"^[a-zA-Z0-9.*]*${0,20}$";
                     String sirCheck = @"([S]+[I]+[R]+[ ]+(([0-2]+[0-9])|([3]+[0-1]))+[\/]+(([0]+[0-9])|([1]+[0-2]))+[\/]+(([0]+[0-9])|([1-2]+[0-9])))";
                     //EMAIL SUBJECT FOR NORMAL EMAIL CHECK STARTS
                     if ((addSubjectBox.Text !="") && (Regex.IsMatch(addSubjectBox.Text, subjectCheck)))
                     {
-                        String contentCheck = @"(.*?){1028}$";
+                        String contentCheck = @"(.*?){0,1028}$";
                         //EMAIL CONTENT FOR NORMAL EMAIL CHECK STARTS
                         if((addContentBox.Text!="") &&(Regex.IsMatch(addContentBox.Text, contentCheck)))
                         {
@@ -93,7 +94,7 @@ namespace NBMFS
 
                     }//EMAIL SUBJECT FOR NORMAL EMAIL CHECK IF ENDS
                     //EMAIL SUBJECT CHECK FOR SIR EMAIL IF STARTS
-                    if((addSubjectBox.Text!="") && (Regex.IsMatch(addSubjectBox.Text, sirCheck)))
+                    else if((addSubjectBox.Text!="") && (Regex.IsMatch(addSubjectBox.Text, sirCheck)))
                     {   
                         //EMAIL CONTENT FOR SIR CHECK STARTS     
                         if(addContentBox.Text!="")
@@ -109,6 +110,11 @@ namespace NBMFS
                             addSenderBox.Text = "";
                             addSubjectBox.Text = "";
                         }//EMAIL CONTENT FOR SIR CHECK ENDS
+                        else
+                        {
+                            statusLbl.Text = "Content empty, please enter SIR code xx-xx-xx followed by a space then the nature of the incident";
+                        }
+
                     }//EMAIL SUBJECT CHECK FOR SIR EMAIL IF ENDS
                     else//SUBJECT CHECK ELSE STARTS
                     {
@@ -131,9 +137,91 @@ namespace NBMFS
 
             //=====================SMS MESSAGE VADLIDATION STARTS==================
             //=====================================================================
-            if ((addIdBox.Text != "") && ((Regex.IsMatch(addIdBox.Text, sAccepted))))
+            else if ((addIdBox.Text != "") && ((Regex.IsMatch(addIdBox.Text, sAccepted))))
             {
 
+                statusLbl.Text = "SMS detected, subject not required.";
+                string senderNo = @"^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$";
+                //SMS SENDER CHECK START
+                if ((addSenderBox.Text != "")&&(Regex.IsMatch(addSenderBox.Text, senderNo)))
+                {
+                    string txtContent = @"(.*?){0,140}";
+                    //SMS CONTENT CHECK START
+                    if((addContentBox.Text != "")&&(Regex.IsMatch(addContentBox.Text, txtContent)))
+                    {
+                        String file = @"textwords.csv";
+                        try
+                        {
+                            MsgList list = MsgList.Instance();
+                            var reader = new StreamReader(File.OpenRead(file));
+                            List<String> rows = new List<string>();
+                            List<String> abvs = new List<string>();
+                            List<String> extends = new List<string>();
+                            while(!reader.EndOfStream)
+                            {
+                                rows.Add(reader.ReadLine());
+                            }
+                            String replace;
+                            foreach(string r in rows)
+                            {
+                                abvs.Add(r.Substring(0, r.IndexOf(",")));
+                                extends.Add(r.Substring(r.IndexOf(",")+1));
+                            }
+
+                            for(int i=0; i<abvs.Count;i++)
+                            {
+                                replace = extends[i];
+                                if (addContentBox.Text.Contains(abvs[i]))
+                                {
+                                    String secondHalf;
+                                    String firstHalf;
+                                    string del = abvs[i];
+
+                                    int j = addContentBox.Text.LastIndexOf(del);
+                                    firstHalf = addContentBox.Text.Substring(0, addContentBox.Text.LastIndexOf(del));
+                                    secondHalf = addContentBox.Text.Remove(addContentBox.Text.LastIndexOf(firstHalf));
+                                    addContentBox.Text = firstHalf + " <" + replace + " >" + secondHalf;
+
+
+                                }
+                            }
+
+                            // abv = rows[i].Substring(0, rows[i].IndexOf(",") - 1);
+                            //expand = rows[i].Substring(rows[i].IndexOf(",") + 1);
+
+
+                            Message sms = new Message(addIdBox.Text, addSenderBox.Text, addSubjectBox.Text, addContentBox.Text);
+                            list.addMessage(sms);
+                            statusLbl.Text = "Message Added!";
+                            addContentBox.Text = "";
+                            addIdBox.Text = "";
+                            addSenderBox.Text = "";
+                            addSubjectBox.Text = "";
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine("File not found");
+                        }
+                        
+
+
+
+
+
+                    }//SMS CONTENT CHECK END
+                    else
+                    {//SMS CONTENT CHECK FOR INVALID ELSE STARTS
+                        statusLbl.Text = "invalid content.";
+                        addContentBox.Text = "";
+                    }//SMS CONTENT CHECK FOR INVALID ELSE ENDS
+                }//SMS SENDER CHECH END
+                else
+                {
+                    statusLbl.Text = "Sender Invalid, please enter valid number";
+                    addSenderBox.Text = "";
+                    addSubjectBox.Text = "";
+                    addContentBox.Text = "";
+                }//SMS SENDER CHECK ELSE ENDS
             }
             //=====================SMS MESSAGE VADLIDATION ENDS====================
             //=====================================================================
